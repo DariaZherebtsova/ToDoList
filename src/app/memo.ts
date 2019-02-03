b6c691bf6855e50f0575503f2c5f209ab225c6e9
Daria Zherebtsova <dd_mail@inbox.ru>
2019-02-03 21:41:48 +0300

    Auto-generated commit

--- src/app/memo.ts
+++
@@ -1,4 +0,0 @@
-export class Memo {
-  done: Boolean = false;
-  text: String;
-}
--- src/app/set-focus.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SetFocusDirective } from './set-focus.directive';
-
-describe('SetFocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SetFocusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
